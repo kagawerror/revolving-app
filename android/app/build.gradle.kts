67a5b61bb5ccc9ceaca64ad95c274fe8206471fd
rev_app <alan.ocsin@brigada.com.ph>
2026-06-03 21:04:10 +0800

    fix(foundation): track app_secrets source; convert Groovy deps block to Kotlin DSL

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,12 +47,12 @@ flutter {
 
 dependencies {
   // Import the Firebase BoM
-  implementation platform('com.google.firebase:firebase-bom:34.14.0')
+  implementation(platform("com.google.firebase:firebase-bom:34.14.0"))
 
 
   // TODO: Add the dependencies for Firebase products you want to use
   // When using the BoM, don't specify versions in Firebase dependencies
-  implementation 'com.google.firebase:firebase-analytics'
+  implementation("com.google.firebase:firebase-analytics")
 
 
   // Add the dependencies for any other desired Firebase products
